3228108126faf9c129e52f104e4f56487f43ef44
muzzamilr <mmuzzamil90@gmail.com>
2023-02-02 20:33:13 +0500

    update tokens

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,14 @@ import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;400&display=swap"
+        rel="stylesheet"
+      />
+    </head>
     <App />
   </React.StrictMode>,
 );
